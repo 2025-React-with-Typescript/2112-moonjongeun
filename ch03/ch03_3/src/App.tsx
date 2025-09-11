dbb80ae1dbe1c2674416c9783d6ac41c2f1d65ea
MJE <moon080108@gmail.com>
2025-09-11 16:52:52 +0900

    1차 과제재출 수정

--- ch03/ch03_3/src/App.tsx
+++ ch03/ch03_3/src/App.tsx
@@ -3,7 +3,7 @@ import ViewportTest from "./pages/ViewportTest";
 import HeightTest from "./pages/HeightTest";
 import PaddingTest from "./pages/PaddingTest";
 import MarginTest from "./pages/MarginTest";
-import ImageTest from "./pages/Imagetest";
+import ImageTest from "./pages/ImageTest";
 import BackgroundImageTest from "./pages/BackgroundImageTest";
 import DisplayTest from "./pages/DisplayTest";
 import DisplayNoneTest from "./pages/DisplayNoneTest";
--- ch03/ch03_3/src/main.tsx
+++ ch03/ch03_3/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import "@fontsource/material-icons";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
